04f8157dd8e2ba833cb7327f97e988fd91870ab3
Manny Venuti <manny.codeschool@gmail.com>
2014-05-04 13:31:35 -0400

    implented Devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+    #required for heroku 
+    #not to set this to your actual host
+  config.action_mailer.default_url_options = { :host => 'http://warm-bayou-4177.herokuapp.com/' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "pages#home"
   get "about" => "pages#about" #Creates about Path
   get "team" => "pages#team"  #Creates Team Path (did this by myself)
